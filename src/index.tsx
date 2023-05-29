0a4883793d7b55dac90a68fa41e6cf0f4d577814
Natalia Kondratieva <kondratieva07@gmail.com>
2023-05-29 11:40:45 +0300

    Monday 2 exam done

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,266 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//1
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// function UsersList() {
+//     const results = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     const users = results[0]
+//     const setUsers = results[1]
+//
+//     return (
+//         <p>Тут будет список пользователей</p>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+//
+// // Какой тип правильнее указать вместо "any" при типизации стэйта?
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//2
+
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     console.log(typeof useState)
+//     return (
+//         <>
+//         <p>Тут будет список пользователей!</p>
+//         </>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList />, document.getElementById('root')
+// );
+//
+// // Что вернёт выражение: typeof useState?
+
+//3
+//
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// function UsersList() {
+//     const results = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//
+//     const users = results[0]
+//     const setUsers = results[1]
+// console.log(results.length)
+//
+//     return (
+//         <p>Тут будет список пользователей</p>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+//
+// // Чему равно results.length?
+
+//4
+//
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     console.log(Array.isArray(users))
+//
+//     return (
+//         <p>Тут будет список пользователей</p>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что вернёт выражение: Array.isArray(users)?
+//
+
+
+//5
+//
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     console.log(typeof setUsers)
+//     return (
+//         <p>Тут будет список пользователей</p>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что вернёт выражение: typeof setUsers?
+
+//6
+//
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 { users.map(user => <li>{user}</li> )}
+//             </ul>
+//         </main>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// Что надо вставить вместо ХХХ, чтобы код заработал?
+
+//7
+//
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     const getUser = (user: string) => <li>{user}</li>
+//     const usersList = users.map(getUser)
+//
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {usersList}
+//             </ul>
+//         </main>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+//
+// // Что надо вставить вместо XXX, чтобы код заработал?
+// // ❗ Ответ дать минимально возможным объёмом кода
+
+//8
+//
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     const getUser = (user: string) => <li>{user}</li>
+//     const usersList = users.map(getUser)
+// console.log(Array.isArray(usersList))
+//
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {usersList}
+//             </ul>
+//         </main>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что вернёт выражение: Array.isArray(usersList)?
+
+//9
+//
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// function UsersList() {
+//     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//     const getUser = (user: string) => <li>{user}</li>
+//     console.log(typeof getUser)
+//
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {users.map(getUser)}
+//             </ul>
+//         </main>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// Что вернёт выражение: typeof getUser?
+
+
+//10
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+//
+// function User(props: UserType) {
+//     return (
+//         <li>User {props.name}: {props.age} y.o.</li>
+//     )
+// }
+//
+// function UsersList() {
+//     const state = [
+//         {id: 1, name: "Bob", age: 34},
+//         {id: 2, name: "Alex", age: 25},
+//         {id: 3, name: "Ann", age: 30},
+//         {id: 4, name: "John", age: 23},
+//     ]
+//     const users = [
+//         {id: 1, userName: "Bob", age: 34},
+//         {id: 2, userName: "Alex", age: 25},
+//         {id: 3, userName: "Ann", age: 30},
+//         {id: 4, userName: "John", age: 23},
+//     ]
+//
+//     const [usersList, setUsersList] = useState<Array<UserType>>(state)
+//     return (
+//         <main>
+//             <h5>User list:</h5>
+//             <p>Тут будет список пользователей</p>
+//         </main>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
+// // Что надо написать вместо XXX, чтобы код работал?
+// // ❗ Если мы отмапим массив, то должны увидеть данные пользователей
+// // ❗ Ответ дать минимально возможным объёмом кода
+
+
+
+
+
